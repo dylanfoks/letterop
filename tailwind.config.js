1fe3bd4c3c1f3cae2065701c7c24e724558c392e
dylanfoks <dafoks@gmail.com>
2021-06-03 18:33:12 +0200

    Added cards and styling, added Info button and page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,27 @@ module.exports = {
     debugScreens: {
       position: ['bottom', 'left'],
     },
-    extend: {},
+    boxShadow: {
+      '2': '0px 4px 8px 0px rgba(0, 0, 0, 0.1)',
+    },
+
+    extend: {
+      colors: {
+        primary:'#1E4BB4',
+        secondary:'#EEE5D6'
+      },
+      fontFamily: {
+        headline: ['OpenSans', 'Helvetica', 'Arial', 'sans-serif']
+      }
+    },
   },
+
   variants: {
     extend: {},
   },
+
   plugins: [
     require('tailwindcss-debug-screens'),
-  ],
+  ]
 }
+
